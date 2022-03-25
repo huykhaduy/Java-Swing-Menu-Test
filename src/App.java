47296a0941f298688c90e0c1abc10e98fb65640f
Huy Kha Duy <huykhaduy@gmail.com>
2022-03-25 09:19:42 +0700

    Add comment for App.java

--- src/App.java
+++ src/App.java
@@ -25,11 +25,18 @@ public class App {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Set JFrame to center
         frame.setLocationRelativeTo(null);
-
+        //Set layout properties
         frame.setLayout(new BorderLayout());
         //Add panel to JFrame
+
+        //************************* Phần chương trình *********************//
+        //Mỗi thành phần trong Menu sẽ được gọi là item
+
+        //Khi click vào item sẽ hiện ra panel bên dưới
         ShowPanel showChild1 = new ShowPanel("Đây là nội dung của child 1");
+        //Tạo ra item thuộc class MenuItem dùng để chứa menu
         MenuItem itemChild = new MenuItem();
+        //Gắn panel hiện ra vào item child
         itemChild.setShowPanel(showChild1);
         itemChild.getLabel().setText("Child1");
         itemChild.setPreferredSize(new Dimension(100, 40));
@@ -45,10 +52,10 @@ public class App {
         itemChild2.setRadius(25);
 
         ShowPanel showMenu1 = new ShowPanel("Đây là nội dung của Menu 1");
+        //Hàm khởi tạo này sẽ các item con của menu, các item con đó sẽ hiện ra khi click vào menu này
         MenuItem itemMenu1 = new MenuItem(itemChild, itemChild2);
         itemMenu1.getLabel().setText("Menu 1");
         itemMenu1.setShowPanel(showMenu1);
-        // itemMenu1.getLabel().setLocation(0, 0);
         itemMenu1.setPreferredSize(new Dimension(100, 40));
         itemMenu1.setBackground(Color.BLUE);
         itemMenu1.setRadius(25);
@@ -70,6 +77,7 @@ public class App {
         itemChild4.setRadius(25);
 
         ShowPanel showMenu2 = new ShowPanel("Đây là nội dung của Menu 2");
+          //Hàm khởi tạo này sẽ các item con của menu, các item con đó sẽ hiện ra khi click vào menu này
         MenuItem itemMenu2 = new MenuItem(itemChild3, itemChild4);
         itemMenu2.getLabel().setText("Menu 2");
         itemMenu2.setShowPanel(showMenu2);
@@ -78,69 +86,53 @@ public class App {
         itemMenu2.setBackground(Color.BLUE);
         itemMenu2.setRadius(25);
 
+        //Tạo panel chứa các menu item
         JPanel jp = new JPanel();
         jp.setLayout(new FlowLayout());
         jp.setBackground(Color.GREEN);
         jp.setPreferredSize(new Dimension(100, 500));
         frame.add(jp, BorderLayout.WEST);
-
         jp.add(itemMenu1);
+        //Thêm khung chứa các menu item là jp
         itemMenu1.addSubItemTo(jp);
         jp.add(itemMenu2);
         itemMenu2.addSubItemTo(jp);
 
-        //Frame ở giữa để show nội dung
-
+        //jp2 là panel dùng để hiện ra nội dung khi click vào menu item
         JPanel jp2 = new JPanel();
         frame.add(jp2, BorderLayout.CENTER);
-        // jp2.setPreferredSize(new Dimension(300, 300));
 
+        // thông báo cho các menu item biết nơi để hiện nội dung
         itemMenu1.addShowItemsTo(jp2);
         itemMenu2.addShowItemsTo(jp2);
-
-        // itemMenu1.add(new JLabel("Hello wrold"));
-       
-        // System.out.println("Number of components: " + itemMenu1.getComponentCount());
-        // jp.addMouseListener(new java.awt.event.MouseAdapter() {
-        //     @Override
-        //     public void mousePressed(MouseEvent e) {
-        //         // TODO Auto-generated method stub
-        //         System.out.println("Mouse Pressed");
-
-        //         super.mousePressed(e);
-        //     }
-        // });
-
-        
-
-
+              
         //Show JFrame
         frame.setVisible(true);
     }
 }
 
-class MousePressedUpdate extends java.awt.event.MouseAdapter{
-    private JPanel panel;
-    //constructor
-    public MousePressedUpdate(JPanel panel) {
-        this.panel = panel;
-    }
+// class MousePressedUpdate extends java.awt.event.MouseAdapter{
+//     private JPanel panel;
+//     //constructor
+//     public MousePressedUpdate(JPanel panel) {
+//         this.panel = panel;
+//     }
 
-    //set getLabel
-    public void setPanel(JPanel panel) {
-        this.panel = panel;
-    }
+//     //set getLabel
+//     public void setPanel(JPanel panel) {
+//         this.panel = panel;
+//     }
 
-    public JPanel getPanel() {
-        return panel;
-    }
+//     public JPanel getPanel() {
+//         return panel;
+//     }
 
-    @Override
-    public void mousePressed(MouseEvent e) {
-        // TODO Auto-generated method stub
-        System.out.println("Mouse Pressed");
+//     @Override
+//     public void mousePressed(MouseEvent e) {
+//         // TODO Auto-generated method stub
+//         System.out.println("Mouse Pressed");
 
-        super.mousePressed(e);
-    }
+//         super.mousePressed(e);
+//     }
 
-}
+// }
